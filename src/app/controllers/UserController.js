4ab120a84bf17557518427a310f8558aa21f2725
gmendes0 <gmendes230@gmail.com>
2019-12-17 02:12:19 -0200

    Added find user and jwt sign from login system

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,11 @@
 const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
+const secret = require('../../config/jwt')
+const jwt = require('jsonwebtoken')
+
+function generateToken(payload = {}) {
+  return jwt.sign(payload, secret.secret, { expiresIn: 86400 })
+}
 
 module.exports = {
   async register(request, response) {
@@ -7,10 +14,31 @@ module.exports = {
 
       const user = await User.create({ email, password })
   
-      return response.json(user)
+      const token = generateToken({ id:  user.id  })
+      return response.json({ user, token })
     } catch (error) {
       console.log(error)
       return response.json({error: error})
     }
   },
+
+  async login(request, response) {
+    try {
+      const { email, password } = request.body
+
+      const user = await User.findOne({ email })
+
+      if (!user)
+        return response.status(404).json({ error: { message: 'user not found.' }})
+      if (!await bcryptjs.compare(password, user.password)) {
+        return response.status(401).json({ error: { message: 'the credentials do not match with our records.' } })
+      } else {
+        const token = generateToken({ id: user.id })
+        return response.json({ user, token })
+      }
+    } catch (error) {
+      console.log(error)
+      return response.json({ error: error.message })
+    }
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const routes = require('express').Router()
 const UserController = require('./app/controllers/UserController')
 
 routes.post('/register', UserController.register)
+routes.post('/login', UserController.login)
 
 module.exports = routes
