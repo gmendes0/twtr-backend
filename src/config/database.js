a4cbf0b917a9da0c4328af19d301b8b9c2a71fd3
gmendes0 <gmendes230@gmail.com>
2019-12-27 03:58:48 -0200

    Using .env on database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config')
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'twtr',
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   define: {
     timestamps: true,
     underscored: true
