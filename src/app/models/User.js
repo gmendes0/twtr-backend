f76123920e933146e6928777541dc3c8ae30354b
gmendes0 <gmendes230@gmail.com>
2020-01-03 17:37:23 -0300

    User validation

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,11 +4,66 @@ const bcryptjs = require('bcryptjs')
 class User extends Model {
   static init(connection) {
     super.init({
-      username: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      name: DataTypes.STRING,
-      birth_date: DataTypes.DATE,
+      username: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'the username can\'t be empty.'
+          },
+          len: {
+            args: [1, 100],
+            msg: 'the username must have between 1 and 100 characters.'
+          },
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'the email can\'t be empty.'
+          },
+          isEmail: {
+            msg: 'invalid email format.'
+          },
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'the password can\'t be empty.'
+          },
+          len: {
+            args: [6, 8],
+            msg: 'the username must have between 6 and 8 characters.'
+          }
+        }
+      },
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'the name can\'t be empty.'
+          },
+          len: {
+            args: {
+              args: [1, 100],
+              msg: 'the name must have between 1 and 100 characters.'
+            }
+          }
+        }
+      },
+      birth_date: {
+        type: DataTypes.DATE,
+        validate: {
+          notEmpty: {
+            msg: 'the birth date can\'t be empty.'
+          },
+          isDate: {
+            msg: 'invalid date.'
+          }
+        }
+      },
     }, {
       hooks: {
         beforeCreate: async (user) => {
